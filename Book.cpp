16e8c09907e2d73bafa4ec696e104077b87aed09
Patrick van Duyse <Pvanduyse@users.noreply.github.com>
2021-09-03 22:37:17 -0600

    Bug 3 (input2.txt)

--- Book.cpp
+++ Book.cpp
@@ -16,6 +16,7 @@ Book::Book(std::string title, std::string author) {
 
 void Book::setGenre(std::string genre) {
     this->genre = genre;
+    genreSet = true; //This line was missing, and so it was never recordded that the genre had been set.
 }
 
 void Book::setPages(std::string pages) {
